6c3ae833d0488730517104ef190282bce2d2c28e
theprivatepark <anneypark@gmail.com>
2020-10-07 13:34:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 5}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.left.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
+
+  if (right > -10) {
+    dodger.style.left = `${right +5}px`
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
